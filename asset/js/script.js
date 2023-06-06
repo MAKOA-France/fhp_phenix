3625fbd9336c829d148aefb87f559a9992574156
Antoine Breheret <breheret@makoa.fr>
2023-06-06 01:39:34 -0400

    mettre en Majuscule le nom de l'entreprise

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,10 +3,7 @@
         attach: function(context, settings) {
             $(document).ready(function(){
                 jQuery('#block-pieddepagemakoa').insertAfter('#bottom_part');
-                console.log('script loaded by module')
             })
         }
     }
 })(jQuery)
-
-console.log('check for ')
