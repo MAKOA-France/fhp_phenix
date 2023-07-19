9ab85637802c7a39eed4c63430378f588832ed23
Antoine Breheret <breheret@makoa.fr>
2023-07-19 13:42:28 +0200

    fix conflict

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
   (function($) {
-=======
-(function($) {
->>>>>>> 85fdbd23b177ba3dad2627bf69e2e2167b53b328
     Drupal.behaviors.fhp = {
         attach: function(context, settings) {
             $(document).ready(function(){
                 jQuery('#block-pieddepagemakoa').insertAfter('#bottom_part');
-<<<<<<< HEAD
                 console.log('script loaded by module')
                 var input = document.querySelector(".crm_phone.twelve.crm-form-text");
                 console.log(input, ' ble input')
@@ -16,13 +11,8 @@
                     initialCountry: "mg",
                 }).setNumber("+261349856896");
                 console.log(input, ' haha')
-=======
->>>>>>> 85fdbd23b177ba3dad2627bf69e2e2167b53b328
             })
         }
     }
 })(jQuery)
-<<<<<<< HEAD
  
-=======
->>>>>>> 85fdbd23b177ba3dad2627bf69e2e2167b53b328
