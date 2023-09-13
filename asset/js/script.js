b271337631b6b2a7fa91400599aed03e8149c456
Dewy <dewymercerais@gmail.com>
2023-09-13 11:06:34 +0200

    refactoring

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,12 +1,14 @@
 //Research page 
 //Get the tbody element 
 let tbodyResearchPage = document.querySelector('table.selector tbody');
+if (tbodyResearchPage) {
 
-// Get all the tr elements inside the tbody
-let rows = tbodyResearchPage.querySelectorAll('tr');
-rows.forEach(row => {
-  const lastTd = row.querySelector('td:last-child');
-  if (lastTd) {
-    lastTd.style.width = 'auto';
-  }
-});
+  // Get all the tr elements inside the tbody
+  let rows = tbodyResearchPage.querySelectorAll('tr');
+  rows.forEach(row => {
+    const lastTd = row.querySelector('td:last-child');
+    if (lastTd) {
+      lastTd.style.width = 'auto';
+    }
+  });
+}
