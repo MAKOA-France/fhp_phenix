d7fb741f4b9c4e0edcd415a3fde7e1e5e51826fb
Dewy <dewymercerais@gmail.com>
2023-09-13 10:59:23 +0200

    rename variable tbody

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,9 +1,9 @@
 //Research page 
 //Get the tbody element 
-const tbody = document.querySelector('table.selector tbody');
+let tbodyResearchPage = document.querySelector('table.selector tbody');
 
 // Get all the tr elements inside the tbody
-const rows = tbody.querySelectorAll('tr');
+let rows = tbodyResearchPage.querySelectorAll('tr');
 rows.forEach(row => {
   const lastTd = row.querySelector('td:last-child');
   if (lastTd) {
