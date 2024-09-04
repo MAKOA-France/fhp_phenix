e3eef67eaa9663ba6cfd84dba9aeab8788d3bbb0
Dewy <dewymercerais@gmail.com>
2024-09-04 13:50:42 +0200

    adding matomo script

--- asset/js/script.js
+++ asset/js/script.js
@@ -17,8 +17,47 @@ if (tbodyResearchPage) {
   $(document).ready(function() {
 
 
+
+
+
+    
+
+
+
+    var matomoScript = `
+    <!-- Matomo -->
+    <script>
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="https://makoa.matomo.cloud/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '11']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src='https://cdn.matomo.cloud/makoa.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <!-- End Matomo Code -->
+  `;
+
+  // Create a new <div> element to hold the script
+  var scriptContainer = $('<div>').html(matomoScript);
+  
+  // Append the script to the <head>
+  $('head').append(scriptContainer.children());
+
+
+
+
+
+
+
+
+
+
     jQuery('.path-frontpage #block-pieddepagemakoa').insertAfter('#bottom_part');
-console.log('checking')
 
 
     // Define the regex pattern to match the URL
